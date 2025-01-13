73fabf52038599f8951f788d7a032e0c4a944891
Khairul122 <khairulhuda242@gmail.com>
2025-01-14 01:21:42 +0700

    update

--- admin/pages/edit-tujuan.php
+++ admin/pages/edit-tujuan.php
@@ -1,96 +1,108 @@
-  <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            Tujuan
-            
-          </h1>
-          <ol class="breadcrumb">
-            <li><a href="home.php"> Home</a></li>
-            <li class="active">Edit Tujuan</li>
-          </ol>
-        </section>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Tujuan
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="home.php">Home</a></li>
+        <li class="active">Edit Tujuan</li>
+    </ol>
+</section>
 
-        <!-- Main content -->
-        <section class="content">
-  <!-- Main row -->
-          <div class="row">
-            <div class="col-xs-12">
-
-              <div class="box">
+<!-- Main content -->
+<section class="content">
+    <!-- Main row -->
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box">
                 <div class="box-header">
-                  <h3 class="box-title">Form Tujuan</h3>
-                 
+                    <h3 class="box-title">Form Tujuan</h3>
                 </div><!-- /.box-header -->
                 <div class="box-body">
-                 <?php 
-                            include './config/koneksi.php';
-
-                            if(isset($_POST['b1'])){
-
-                                
-                            if(empty($_POST['nm']) or empty($_POST['lm'])){
-
-                            echo '<div class="alert alert-warning alert-dismissible fade in" role="alert">
-                                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                  <span aria-hidden="true">×</span></button>
-                                  <strong>Error!</strong> Data tidak boleh ada yang kosong.
-                                  </div>';
-
-                            }else{
-                                 $sql=mysqli_query($koneksi,"UPDATE tujuan SET nama_tujuan='$_POST[nm]',lama_tujuan='$_POST[lm]' WHERE kode_tujuan='$_POST[kd]'");
-                                
-
-                                 echo '<div class="alert alert-success alert-dismissible fade in" role="alert">
-                                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                  <span aria-hidden="true">×</span></button>
-                                  <strong>Sukses!</strong> Tujuan berhasil diedit.
+                    <?php 
+                        include './config/koneksi.php';
+                        if(isset($_GET['id'])) {
+                            $id = $_GET['id'];
+                            $sqps = mysqli_query($koneksi, "SELECT * FROM tujuan WHERE kode_tujuan = '$id'");
+                            if($ps = mysqli_fetch_array($sqps)) {
+                                if(isset($_POST['b1'])){
+                                    if(empty($_POST['nm']) or empty($_POST['lm']) or empty($_POST['pelabuhan_asal'])){
+                                        echo '<div class="alert alert-warning alert-dismissible fade in" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">×</span></button>
+                                            <strong>Error!</strong> Semua data harus diisi.
+                                            </div>';
+                                    } else {
+                                        $sql = mysqli_query($koneksi, "UPDATE tujuan SET nama_tujuan = '$_POST[nm]', lama_tujuan = '$_POST[lm]', pelabuhan_asal = '$_POST[pelabuhan_asal]' WHERE kode_tujuan = '$_POST[kd]'");
+                                        if($sql){
+                                            echo '<div class="alert alert-success alert-dismissible fade in" role="alert">
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">×</span></button>
+                                                <strong>Sukses!</strong> Tujuan berhasil diedit.
+                                                </div>';
+                                        } else {
+                                            echo '<div class="alert alert-danger alert-dismissible fade in" role="alert">
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">×</span></button>
+                                                <strong>Error!</strong> ' . mysqli_error($koneksi) . '
+                                                </div>';
+                                        }
+                                    }
+                                }
+                                ?>
+                                <form id="contactForm" action="" method="post">
+                                    <div class="row">
+                                        <div class="form-group">
+                                            <div class="col-md-12">
+                                                <label>Kode Tujuan</label>
+                                                <input type="text" name="kd" class="form-control" value="<?php echo $_GET['id']; ?>" readonly>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="row">
+                                        <div class="form-group">
+                                            <div class="col-md-6">
+                                                <label>Nama Tujuan</label>
+                                                <input type="text" name="nm" class="form-control" value="<?php echo $ps['nama_tujuan']; ?>">
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <label>Lama Tujuan</label>
+                                                <input type="text" name="lm" class="form-control" value="<?php echo $ps['lama_tujuan']; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="row">
+                                        <div class="form-group">
+                                            <div class="col-md-6">
+                                                <label>Pelabuhan Asal</label>
+                                                <input type="text" name="pelabuhan_asal" class="form-control" value="<?php echo $ps['pelabuhan_asal']; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <input type="submit" name="b1" class="btn btn-primary" value="Edit Tujuan">
+                                            <a href="home.php?p=tujuan" class="btn btn-success">Kembali</a>
+                                        </div>
+                                    </div>
+                                </form>
+                                <?php
+                            } else {
+                                echo '<div class="alert alert-danger">
+                                    <strong>Error!</strong> Tujuan tidak ditemukan.
                                   </div>';
                             }
-                            }
-                            ?>
-                     <form id="contactForm" action="" method="post">
-                      <?php  
-                                     
-                      $id=$_GET['id'];
-                      $sqps=mysqli_query($koneksi,"SELECT * FROM tujuan where kode_tujuan='$id'");
-                      $ps=mysqli_fetch_array($sqps);
-
-                      ?>
-                     <div class="row">
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                     <label>Kode Tujuan</label>
-                                        <input type="text" name="kd" class="form-control" maxlength="100" value="<?php echo $_GET['id']; ?>" placeholder="Kode Tujuan" readonly>
-                                    </div>
-                                     
-                                </div>
-                            </div>
-                            <br>
-                             <div class="row">
-                                <div class="form-group">
-                                    <div class="col-md-6">
-                                     <label>Nama Tujuan</label>
-                                        <input type="text" name="nm" class="form-control" maxlength="100" value="<?php echo $ps['nama_tujuan']; ?>" placeholder="Nama Tujuan">
-                                    </div>
-                                      <div class="col-lg-6 ">
-                                        <label>Lama Tujuan</label>
-                                        <input type="text" name="lm" class="form-control" maxlength="100" value="<?php echo $ps['lama_tujuan']; ?>" placeholder="Lama tujuan">
-                                    </div>
-                                </div>
-                            </div>
-                            <br>
-                           
-                              <br>
-                           <div class="row">
-                                <div class="col-md-12">
-                                    <input type="submit" name="b1" data-loading-text="Loading..." class="btn btn-primary" value="Edit Tujuan">
-                                     <a href="home.php?p=tujuan" class="btn btn-success"> Kembali </a>
-                                </div>
-                            </div>
-                        </form>
+                        } else {
+                            echo '<div class="alert alert-danger">
+                                <strong>Error!</strong> Tidak ada kode tujuan yang spesifik.
+                              </div>';
+                        }
+                    ?>
                 </div><!-- /.box-body -->
-              </div><!-- /.box -->
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-
-     </section><!-- /.content -->
+            </div><!-- /.box -->
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+</section><!-- /.content -->
--- admin/pages/tambah-tujuan.php
+++ admin/pages/tambah-tujuan.php
@@ -1,97 +1,98 @@
-  <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            Tujuan
-            
-          </h1>
-          <ol class="breadcrumb">
-            <li><a href="home.php"> Home</a></li>
-            <li class="active">Tambah Tujuan</li>
-          </ol>
-        </section>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>Tujuan</h1>
+  <ol class="breadcrumb">
+    <li><a href="home.php">Home</a></li>
+    <li class="active">Tambah Tujuan</li>
+  </ol>
+</section>
 
-        <!-- Main content -->
-        <section class="content">
+<!-- Main content -->
+<section class="content">
   <!-- Main row -->
-          <div class="row">
-            <div class="col-xs-12">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Form Tujuan</h3>
+        </div><!-- /.box-header -->
+        <div class="box-body">
+          <?php
+          include './config/koneksi.php';
 
-              <div class="box">
-                <div class="box-header">
-                  <h3 class="box-title">Form Tujuan</h3>
-                 
-                </div><!-- /.box-header -->
-                <div class="box-body">
-                 <?php 
-                            include './config/koneksi.php';
+          $auto = "001";
+          $read = mysqli_query($koneksi, "SELECT right(kode_tujuan,3) FROM tujuan ORDER BY kode_tujuan DESC");
+          if ($rec = mysqli_fetch_array($read)) {
+            $auto = $rec[0] + 1;
+            if ($auto < 100) $auto = "0" . $auto;
+            if ($auto < 10) $auto = "0" . $auto;
+          }
+          $_POST['id'] = "IT-B" . $auto;
 
-                             $auto="001";
-                                  $read=mysqli_query($koneksi,"select right(kode_tujuan,3) from tujuan order by kode_tujuan desc");
-                                  if ($rec=mysqli_fetch_array($read)){
-                                  $auto=$rec[0]+1;
-                                  if ($auto<100) $auto="0".$auto;
-                                  if ($auto<10) $auto="0".$auto;
-                                  }
-                                  $_POST['id']="IT-B".$auto;
-
-                            if(isset($_POST['b1'])){
-
-                            if(empty($_POST['nm']) or empty($_POST['lm'])){
-
-                            echo '<div class="alert alert-warning alert-dismissible fade in" role="alert">
-                                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                  <span aria-hidden="true">×</span></button>
-                                  <strong>Error!</strong> Data tidak boleh ada yang kosong.
-                                  </div>';
-
-                            }else{
-                                $sql=mysqli_query($koneksi,"INSERT INTO tujuan values ('$_POST[id]','$_POST[nm]','$_POST[lm]')");
-                                
-
-                                 echo '<div class="alert alert-success alert-dismissible fade in" role="alert">
-                                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                  <span aria-hidden="true">×</span></button>
-                                  <strong>Sukses!</strong> Tujuan berhasil ditambah.
-                                  </div>';
-                            }
-                            }
-                            ?>
-                     <form id="contactForm" action="" method="post">
-                     <div class="row">
-                                <div class="form-group">
-                                    <div class="col-md-12">
-                                     <label>ID Tujuan</label>
-                                        <input type="text" name="id" class="form-control" maxlength="100" value="<?php echo $_POST['id']; ?>" placeholder="ID Tujuan" readonly>
-                                    </div>
-                                      
-                                </div>
-                            </div>
-                            <br>
-                             <div class="row">
-                                <div class="form-group">
-                                    <div class="col-md-6">
-                                     <label>Nama Tujuan</label>
-                                        <input type="text" name="nm" class="form-control" maxlength="100" value="" placeholder="Nama Tujuan">
-                                    </div>
-                                      <div class="col-lg-6 ">
-                                        <label>Lama Tujuan</label>
-                                        <input type="text" name="lm" class="form-control" maxlength="100" value="" placeholder="Lama tujuan">
-                                    </div>
-                                </div>
-                            </div>
-                            <br>
-                           
-                              <br>
-                           <div class="row">
-                                <div class="col-md-12">
-                                    <input type="submit" name="b1" data-loading-text="Loading..." class="btn btn-primary" value="Tambah Tujuan">
-                                     <a href="home.php?p=tujuan" class="btn btn-success"> Kembali </a>
-                                </div>
-                            </div>
-                        </form>
-                </div><!-- /.box-body -->
-              </div><!-- /.box -->
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-
-     </section><!-- /.content -->
+          if (isset($_POST['b1'])) {
+            if (empty($_POST['nm']) or empty($_POST['lm']) or empty($_POST['pelabuhan_asal'])) {
+              echo '<div class="alert alert-warning alert-dismissible" role="alert">
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+                  <strong>Error!</strong> Data tidak boleh ada yang kosong.
+              </div>';
+            } else {
+              $sql = "INSERT INTO tujuan (kode_tujuan, nama_tujuan, lama_tujuan, pelabuhan_asal) VALUES ('" . $_POST['id'] . "', '" . $_POST['nm'] . "', '" . $_POST['lm'] . "', '" . $_POST['pelabuhan_asal'] . "')";
+              if (mysqli_query($koneksi, $sql)) {
+                echo '<div class="alert alert-success alert-dismissible" role="alert">
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+                      <strong>Sukses!</strong> Tujuan berhasil ditambah.
+                  </div>';
+              } else {
+                echo '<div class="alert alert-danger alert-dismissible" role="alert">
+                      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+                      <strong>Error!</strong> Gagal menambahkan tujuan: ' . mysqli_error($koneksi) .
+                  '</div>';
+              }
+            }
+          }
+          ?>
+          <form id="contactForm" action="" method="post">
+            <div class="row">
+              <div class="form-group">
+                <div class="col-md-12">
+                  <label>ID Tujuan</label>
+                  <input type="text" name="id" class="form-control" maxlength="100" value="<?php echo isset($_POST['id']) ? $_POST['id'] : ''; ?>" placeholder="ID Tujuan" readonly>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="form-group">
+                <div class="col-md-6">
+                  <label>Nama Tujuan</label>
+                  <input type="text" name="nm" class="form-control" maxlength="100" placeholder="Nama Tujuan">
+                </div>
+                <div class="col-lg-6">
+                  <label>Lama Tujuan</label>
+                  <input type="text" name="lm" class="form-control" maxlength="100" placeholder="Lama Tujuan">
+                </div>
+              </div>
+            </div>
+            <br>
+            <!-- Additional form for Pelabuhan Asal -->
+            <div class="row">
+              <div class="form-group">
+                <div class="col-md-6">
+                  <label>Pelabuhan Asal</label>
+                  <input type="text" name="pelabuhan_asal" class="form-control" maxlength="100" placeholder="Pelabuhan Asal">
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="col-md-12">
+                <input type="submit" name="b1" class="btn btn-primary" value="Tambah Tujuan">
+                <a href="home.php?p=tujuan" class="btn btn-success">Kembali</a>
+              </div>
+            </div>
+          </form>
+        </div><!-- /.box-body -->
+      </div><!-- /.box -->
+    </div><!-- /.col -->
+  </div><!-- /.row -->
+</section><!-- /.content -->
